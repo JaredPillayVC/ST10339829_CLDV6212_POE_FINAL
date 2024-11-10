3a96a8687455ebbc53e2ef01fe2e342be57c4ad8
GAMESGLOBALOPER\jared.pillay <jared.pillay@gamesglobal.com>
2024-11-10 19:28:16 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10339829_CLDV6212_POE_FINAL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
